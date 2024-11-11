db80e99edf0f1a6ece22a4e83457ca70dce417e3
cl-cassanelli <cldevofficial@gmail.com>
2024-11-11 11:59:50 +0100

    Push

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -1,2 +1,137 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TestGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1) Multipli");
+            Console.WriteLine("2) Fattoriale");
+            Console.WriteLine("3) Numeri primi");
+            Console.WriteLine("4) Somma numeri");
+            Console.WriteLine("5) Conta vocali");
+            Console.Write("Scegli un programma: ");
+            int scelta = int.Parse(Console.ReadLine());
+            int numero = 0;
+
+            switch (scelta)
+            {
+                case 1:
+                    // Calcolo multipli fino a 100
+                    Console.Write("Inserisci un numero: ");
+                    numero = int.Parse(Console.ReadLine());
+                    Multipli(numero);
+                    break;
+
+                case 2:
+                    // Calcolare il fattoriale di un numero
+                    Console.Write("Inserisci un numero: ");
+                    numero = int.Parse(Console.ReadLine());
+                    Fattoriale(numero);
+                    break;
+                case 3:
+                    // Verificare se un numero è primo
+                    Console.Write("Inserisci un numero: ");
+                    numero = int.Parse(Console.ReadLine());
+
+                    bool isPrimo = Primi(numero);
+                    if (isPrimo)
+                        Console.WriteLine($"{numero} è un numero primo.");
+                    else
+                        Console.WriteLine($"{numero} non è un numero primo.");
+                    break;
+                case 4:
+                    // Somma numeri pari fino a 100
+                    Console.Write("Inserisci un numero: ");
+                    numero = int.Parse(Console.ReadLine());
+                    SommaPari(numero);
+                    break;
+                case 5:
+                    // Conta vocali nelle frasi
+                    Console.Write("Inserisci una frase: ");
+                    string frase = Console.ReadLine();
+                    ContaVocali(frase);
+                    break;
+
+                default:
+                    Console.WriteLine("Scelta non valida.");
+                    break;
+            }
+        }
+
+        public static void Multipli(int numero)
+        {
+            Console.WriteLine($"Multipli di {numero} fino a 100:");
+
+            for (int i = 1; i <= 100; i++)
+            {
+                int multiplo = numero * i;
+                if (multiplo > 100)
+                    break;
+
+                Console.WriteLine(multiplo);
+            }
+        }
+
+        public static void Fattoriale(int numero)
+        {
+            Console.Write($"Fattoriale di {numero}: ");
+
+            long fattoriale = 1;
+            if (numero == 0) fattoriale = 0;
+
+            for (int i = numero; i > 1; i--)
+            {
+                fattoriale *= i;
+            }
+
+            Console.Write(fattoriale);
+        }
+
+        public static bool Primi(int numero)
+        {
+            if(numero < 2) return false;
+
+            for (int i = 2; i <= Math.Sqrt(numero); i++)
+            {
+                if (numero % i == 0)
+                    return false; 
+            }
+            return true; 
+
+        }
+
+        public static void SommaPari(int numero)
+        {
+            int somma = 0;
+            Console.WriteLine($"Numeri pari fino a {numero}");
+            for (int i = 0; i < numero; i++) 
+            {
+                if (i % 2 == 0)
+                {
+                    somma += i;
+                }
+            }
+            Console.WriteLine($"La somma dei numeri pari fino a {numero} (escluso) è: {somma}");
+        }
+
+        public static void ContaVocali(string frase)
+        {
+            string vocali = "aeiouAEIOU";
+            int count = 0;
+
+            foreach(char lettera in frase)
+            {
+                if(vocali.Contains(lettera))
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+
+
+    }
+}
--- TestGit/obj/Debug/net8.0/TestGit.AssemblyInfo.cs
+++ TestGit/obj/Debug/net8.0/TestGit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestGit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9fd83be6e342db473770a0ac8c8f703cd4ba2b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestGit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestGit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
